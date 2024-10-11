d99df759ef4f8abb04f0ab7357d50f76751b532b
vkinnune <valtteri@tekve.fi>
2024-10-11 15:07:51 +0300

    fixed

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,17 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+  },
   palette: {
     primary: {
-      main: "#1976d2",
+      main: '#1976d2',
     },
     secondary: {
-      main: "#dc004e",
+      main: '#dc004e',
     },
   },
-});
+})
 
-export default theme;
+export default theme
